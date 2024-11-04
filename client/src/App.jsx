f76a04f9819d8c50d43b702e1b23a6c940d02b52
Aykın Sancaklı <sancakliaykin@gmail.com>
2024-11-04 16:34:49 +0300

    test api for menu

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,26 @@
 import { useEffect, useState } from 'react';
 
+const API_URL = import.meta.env.VITE_SERVER_DEVELOPMENT_URL;
+
 function App() {
-  const [fruits, setFruits] = useState([]);
+  const [menu, setMenu] = useState([]);
 
   useEffect(function () {
     async function fetchFruits() {
-      const res = await fetch(`http://localhost:8080/api`);
+      const res = await fetch(`${API_URL}/menu`);
       const data = await res.json();
-      setFruits(data.fruits);
+      console.log(data);
+      setMenu(data);
     }
     fetchFruits();
   }, []);
 
   return (
     <div className='mx-auto w-96 bg-slate-200 text-xl text-red-500'>
-      {fruits.map((fruit, i) => (
-        <li className='list-none' key={i}>
-          {fruit}
-        </li>
+      {menu.map((item) => (
+        <p key={item.id}>
+          {item.name}, {item.id}
+        </p>
       ))}
     </div>
   );
