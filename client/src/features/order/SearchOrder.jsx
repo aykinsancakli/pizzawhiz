a2b0837fd763b7f40f56ea02d1d0c603ecd4dc18
Aykın Sancaklı <sancakliaykin@gmail.com>
2024-11-05 21:02:00 +0300

    Change logo color

--- client/src/features/order/SearchOrder.jsx
+++ client/src/features/order/SearchOrder.jsx
@@ -11,7 +11,7 @@ function SearchOrder() {
         placeholder='Search order #'
         value={query}
         onChange={(e) => setQuery(e.target.value)}
-        className='w-[150px] rounded-full bg-orange-100 px-4 py-2 text-sm outline-none transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-orange-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72'
+        className='w-[150px] rounded-full bg-orange-100 px-4 py-2 text-sm outline-none transition-all duration-300 placeholder:text-stone-400 focus:outline-none focus:ring focus:ring-orange-500 focus:ring-opacity-50 sm:w-64 sm:focus:w-72 lg:w-96'
       ></input>
     </form>
   );
--- client/src/ui/Header.jsx
+++ client/src/ui/Header.jsx
@@ -4,7 +4,7 @@ import Username from '../features/user/Username';
 
 function Header() {
   return (
-    <header className='flex items-center justify-between px-4 py-3 text-orange-100 uppercase bg-orange-400 border-b border-stone-200 sm:px-6'>
+    <header className='flex items-center justify-between px-4 py-3 uppercase bg-orange-400 border-b border-stone-200 text-orange-50 sm:px-6'>
       <Link to='/' className='tracking-widest'>
         PizzaWhiz Co.
       </Link>
