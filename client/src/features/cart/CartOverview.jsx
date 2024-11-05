992f8744d81aafb682f9a375905ae7e239431c2b
Aykın Sancaklı <sancakliaykin@gmail.com>
2024-11-05 14:25:12 +0300

    Create cart overview

--- client/src/features/cart/CartOverview.jsx
+++ client/src/features/cart/CartOverview.jsx
@@ -1,8 +1,13 @@
+import { Link } from 'react-router-dom';
+
 function CartOverview() {
   return (
-    <div className='flex h-16 items-center justify-between bg-gray-700 px-5 py-3'>
-      <span>3 pizzas</span>
-      <span>Go to cart</span>
+    <div className='flex items-center justify-between px-4 py-4 text-sm uppercase bg-stone-800 text-stone-200 sm:px-6 md:text-base'>
+      <p className='space-x-4 font-semibold text-stone-300'>
+        <span>23 pizzas</span>
+        <span>147€</span>
+      </p>
+      <Link to='/cart'>Open cart &rarr;</Link>
     </div>
   );
 }
