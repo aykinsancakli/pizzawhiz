b883da9caa79d0e642005dcef9eafe4c2c7bafc6
Aykın Sancaklı <sancakliaykin@gmail.com>
2024-11-06 02:36:21 +0300

    Implement create order functionality

--- client/src/features/order/CreateOrder.jsx
+++ client/src/features/order/CreateOrder.jsx
@@ -1,7 +1,13 @@
-import { Form } from 'react-router-dom';
+import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import Button from '../../ui/Button';
-import { useSelector } from 'react-redux';
-import { getCart } from '../cart/cartSlice';
+import { useDispatch, useSelector } from 'react-redux';
+import { clearCart, getCart, getTotalCartPrice } from '../cart/cartSlice';
+import { useState } from 'react';
+import EmptyCart from '../cart/EmptyCart';
+import { fetchAddress } from '../user/userSlice';
+import { formatCurrency } from '../../utils/helpers';
+import { createOrder } from '../../services/apiRestaurant';
+import store from '../../store';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -10,6 +16,29 @@ const isValidPhone = (str) =>
   );
 
 function CreateOrder() {
+  const [withPriority, setWithPriority] = useState(false);
+  const {
+    username,
+    status: addressStatus,
+    position,
+    address,
+    error: errorAddress,
+  } = useSelector((state) => state.user);
+  const isLoadingAddress = addressStatus === 'loading';
+
+  const navigation = useNavigation();
+  const isSubmitting = navigation.state === 'submitting';
+
+  const formErrors = useActionData();
+  const dispatch = useDispatch();
+
+  const cart = useSelector(getCart);
+  const totalCartPrice = useSelector(getTotalCartPrice);
+  const priorityPrice = withPriority ? totalCartPrice * 0.2 : 0;
+  const totalPrice = totalCartPrice + priorityPrice;
+
+  if (!cart.length) return <EmptyCart />;
+
   return (
     <div className='px-4 py-6'>
       <h2 className='mb-8 text-xl font-semibold'>
@@ -25,6 +54,7 @@ function CreateOrder() {
               className='w-full input'
               type='text'
               name='customer'
+              defaultValue={username}
               required
             />
           </div>
@@ -34,6 +64,11 @@ function CreateOrder() {
           <label className='sm:basis-40'>Phone number</label>
           <div className='grow'>
             <input className='w-full input' type='tel' name='phone' required />
+            {formErrors?.phone && (
+              <p className='p-2 mt-2 text-xs text-red-700 bg-red-100 rounded-md'>
+                {formErrors.phone}
+              </p>
+            )}
           </div>
         </div>
 
@@ -44,9 +79,31 @@ function CreateOrder() {
               className='w-full input'
               type='text'
               name='address'
+              disabled={isLoadingAddress}
+              defaultValue={address}
               required
             />
+            {addressStatus === 'error' && (
+              <p className='p-2 mt-2 text-xs text-red-700 bg-red-100 rounded-md'>
+                {errorAddress}
+              </p>
+            )}
           </div>
+
+          {!position.latitude && !position.longitude && (
+            <span className='md:right[-5px] absolute right-[3px] top-[3px] z-50 md:top-[5px]'>
+              <Button
+                disabled={isLoadingAddress}
+                type='small'
+                onClick={(e) => {
+                  e.preventDefault();
+                  dispatch(fetchAddress());
+                }}
+              >
+                Get position
+              </Button>
+            </span>
+          )}
         </div>
 
         <div className='flex items-center gap-5 mb-12'>
@@ -55,6 +112,8 @@ function CreateOrder() {
             type='checkbox'
             name='priority'
             id='priority'
+            value={withPriority}
+            onChange={(e) => setWithPriority(e.target.checked)}
           />
           <label htmlFor='priority' className='font-medium'>
             Want to yo give your order priority?
@@ -62,15 +121,52 @@ function CreateOrder() {
         </div>
 
         <div>
-          <input type='hidden' name='cart' />
+          <input type='hidden' name='cart' value={JSON.stringify(cart)} />
+          <input
+            type='hidden'
+            name='position'
+            value={
+              position.longitude && position.latitude
+                ? `${position.latitude},${position.longitude}`
+                : ''
+            }
+          />
 
-          <Button type='primary'>Order now</Button>
+          <Button disabled={isSubmitting || isLoadingAddress} type='primary'>
+            {isSubmitting
+              ? 'Placing order...'
+              : `Order now for ${formatCurrency(totalPrice)}`}
+          </Button>
         </div>
       </Form>
     </div>
   );
 }
 
-export async function action({ request }) {}
+export async function action({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+
+  const order = {
+    ...data,
+    cart: JSON.parse(data.cart),
+    priority: data.priority === 'true',
+  };
+
+  const errors = {};
+  if (!isValidPhone(order.phone))
+    errors.phone =
+      'Please give us your correct phone number. We might need it to contact you.';
+
+  if (Object.keys(errors).length > 0) return errors;
+
+  // If everything is okay, create new order and redirect
+  const newOrder = await createOrder(order);
+
+  // Do not overuse
+  store.dispatch(clearCart());
+
+  return redirect(`/order/${newOrder.id}`);
+}
 
 export default CreateOrder;
--- client/src/features/user/userSlice.js
+++ client/src/features/user/userSlice.js
@@ -1,7 +1,38 @@
-import { createSlice } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { getAddress } from '../../services/apiGeocoding';
+
+function getPosition() {
+  return new Promise(function (resolve, reject) {
+    navigator.geolocation.getCurrentPosition(resolve, reject);
+  });
+}
+
+export const fetchAddress = createAsyncThunk(
+  'user/fetchAddress',
+  async function () {
+    // 1) Get user's geolocation position
+    const positionObj = await getPosition();
+    const position = {
+      latitude: positionObj.coords.latitude,
+      longitude: positionObj.coords.longitude,
+    };
+
+    // 2) Reverse geocoding
+    const addressObj = await getAddress(position);
+    const address = `${addressObj?.locality}, ${addressObj?.city} ${addressObj?.postcode}, ${addressObj?.countryName}`;
+
+    // 3) Return an object with the data we interested in
+    // Payload of the FULFILLED state
+    return { position, address };
+  },
+);
 
 const initialState = {
   username: '',
+  status: 'idle',
+  position: {},
+  address: '',
+  error: '',
 };
 
 const userSlice = createSlice({
@@ -12,6 +43,21 @@ const userSlice = createSlice({
       state.username = action.payload;
     },
   },
+  extraReducers: (builder) =>
+    builder
+      .addCase(fetchAddress.pending, (state, action) => {
+        state.status = 'loading';
+      })
+      .addCase(fetchAddress.fulfilled, (state, action) => {
+        state.position = action.payload.position;
+        state.address = action.payload.address;
+        state.status = 'idle';
+      })
+      .addCase(fetchAddress.rejected, (state, action) => {
+        (state.status = 'error'),
+          (state.error =
+            'There was a problem getting your address. Make sure to fill this field!');
+      }),
 });
 
 export const { updateName } = userSlice.actions;
